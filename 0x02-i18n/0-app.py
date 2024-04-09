c1ce2f03e5b10fd90be4c53382e46daabc9674ca
HEMAN-DIARCK <hemanlochaa2@gmail.com>
2024-04-09 16:17:36 +0300

    update

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,12 @@
+#!usr/bin/env python3
+''' Task 0: Basic Flask app
+'''
+from flask import Flask, render_template
+
+app = Flask(__neme__)
+
+
+@app.route('/')
+def index():
+    '''default route'''
+    return render_template("0-index.html",)
